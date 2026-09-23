3d5af40605ed2f393ec65d0cf91c0432ff44d1cf
Bogdan Agafonov <bogdan.agafonov@gmail.com>
2026-09-23 16:29:14 +0200

    Bundle shared Match site styles and icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./vendor/berlin-tower.js"
 import { createApp } from "vue"
 import App from "./App.vue"
+import "./vendor/site-foundation.css"
 import "./style.css"
 
 createApp(App).mount("#app")
